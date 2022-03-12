0f790fa193bf8d572a11ad754284a260de377237
chrisurf <chris.oguntolu@gmail.com>
2022-03-12 19:12:39 +0100

    :rocket: mysql docker container

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 echo "Starting database container.."
 docker-compose up -d
 
-sleep 3
-echo "Launching phpmyadmin.."
-open http://localhost:8081
+while true; do
+    read -p "Do you want to connect to the mysql server? " yn
+    case $yn in
+        [Yy]* ) echo "Launching phpmyadmin..";
+                open http://localhost:8081; 
+                echo "Connecting to database server..";
+                docker exec -it mysql-container /bin/sh;
+                break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
-echo "Connecting to database server.."
-docker exec -it mysql-container /bin/sh
