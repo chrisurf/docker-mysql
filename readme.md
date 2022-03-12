b55a9f4d7e15afa0ccac71bede3542a23dbf464b
chrisurf <chris.oguntolu@gmail.com>
2022-03-12 19:23:13 +0100

    :rocket: mysql docker container

--- readme.md
+++ readme.md
@@ -110,5 +110,5 @@ $ docker-compose up
 Connect to the database server
 
 ```bash
-$ docker exec -it mysql-container /bin/sh
+$ docker exec -it mysql-mysql-container /bin/sh
 ```
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ while true; do
         [Yy]* ) echo "Launching phpmyadmin..";
                 open http://localhost:8081; 
                 echo "Connecting to database server..";
-                docker exec -it mysql-container /bin/sh;
+                docker exec -it mysql-mysql-container /bin/sh;
                 break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
